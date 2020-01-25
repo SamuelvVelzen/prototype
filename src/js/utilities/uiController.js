497449fc93f6a9f076f99f1b257dbd13b8817b64
Samuel <samuelvvelzen@gmail.com>
2020-01-25 21:24:57 +0100

    adding more identifiers to uicontroller

--- src/js/utilities/uiController.js
+++ src/js/utilities/uiController.js
@@ -2,6 +2,10 @@ var uiController = (function() {
     var uiStrings = {
         id: {
             notification: 'notification'
+        },
+        class: {
+            filter_item: 'filterbalk_container_item',
+            info_item: 'infocard_inner_body_item'
         }
     };
 
