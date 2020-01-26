a87da03d206020d5835f52ee2e0b73f5c3e160f3
Samuel <samuelvvelzen@gmail.com>
2020-01-26 19:58:57 +0100

    adding more identifiers and new object for pages

--- src/js/utilities/uiController.js
+++ src/js/utilities/uiController.js
@@ -1,14 +1,20 @@
 var uiController = (function() {
     var uiStrings = {
         id: {
-            notification: 'notification'
+            notification: 'notification',
+            generateTitle: 'generateTitle',
+            generateButton: 'generateArticle'
         },
         class: {
             filter_item: 'filterbalk_container_item',
-            info_item: 'infocard_inner_body_item'
+            info_item: 'infocard_inner_body_item',
+            close: 'close'
         },
         style: {
             order: 'flex_order'
+        },
+        pages: {
+            article: 'article.html'
         }
     };
 
